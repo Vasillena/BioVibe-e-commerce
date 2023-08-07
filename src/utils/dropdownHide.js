db92db50e40890de7b5be81ddb0544bfc9582b9d
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2023-08-07 16:28:44 +0300

    Update dropdownHide.js

--- src/utils/dropdownHide.js
+++ src/utils/dropdownHide.js
@@ -9,14 +9,12 @@ export function dropdownHide() {
       dropdown.addEventListener("click", (event) => {
         if (event.target.tagName === "A") {
           dropdown.style.display = "none";
-          dropdown.style.visibility = "hidden";
         }
       });
 
       productsLink.addEventListener("mouseenter", () => {
         dropdown.style.display = "block";
-        dropdown.style.visibility = "visible";
       });
-    }, 1000);
+    }, 100);
   }
 }
